238a11a90ea1dbcae2aa29d0975c8fc0048f226b
IgorRomG3 <webroma57@gmail.com>
2017-02-07 00:23:54 +0200

    added the ability to display to the console the dictionary, fixes for the convenience of work, fixed readme information

---
+++ main.js
@@ -0,0 +1,47 @@
+var englishWords = [];
+var rusWords     = [];
+var rand         =0;
+
+while(true) {
+  var enterEnglish = prompt("Enter English word");
+    if(enterEnglish===null) {
+      alert('Vocabulary is ready for studying');
+      break;
+    }
+  var enterRus = prompt("Enter translation of previous English word");
+    if(enterRus===null) {
+      alert('Vocabulary is ready for studying');
+      break;
+    }
+  englishWords.push(enterEnglish);
+  rusWords.push(enterRus);
+}
+for (var i = 0; i < englishWords.length; i++) {
+  for (var j = 0; j <= rusWords.length; j++) {
+    if (i===j) {
+      console.log(englishWords[i]+ " : " +rusWords[j]);
+      break;
+    }
+  }
+}
+outer:while (true) {
+  rand = Math.floor(Math.random()*(englishWords.length));
+  var englishWord = prompt('How to translate: ' + englishWords[rand] + "?");
+  if(englishWord===null) {
+    alert('Goodbye');
+    break;
+  }
+  if(englishWord!==rusWords[rand]) {
+    while (englishWord!==rusWords[rand]) {
+      var englishWord = prompt('INCORRECT,try again. How to translate: ' + englishWords[rand] + "?");
+      if(englishWord===null) {
+        alert('Goodbye');
+        break outer;
+      }else {
+        alert('Correct! Translation is: ' + rusWords[rand]);
+      }
+    }
+  }else {
+    alert('Correct! Translation is: ' + rusWords[rand]);
+  }
+}
